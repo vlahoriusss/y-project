b67c05ce35e41851f84a874932975f3f6708cdee
vlahorius <127651785+vlahoriusss@users.noreply.github.com>
2024-04-14 21:54:03 +0300

    t2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
-import { AppProps } from "next/app";
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
+import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
+import "@/styles/globals.css";
+import EditModal from "@/components/modals/EditModal";
+import PostModal from "@/components/modals/PostModal";
 
-export default function App({ pageProps }: AppProps) {
-  const { error } = pageProps;
-
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-        <div>There's something wrong with your Proxima account, check back soon</div>
-    </div>
+<div>
+  <h1 className="text-white text-xl">There's Something Wrong with your Proxima Account. </h1>
+</div>
   );
 }
