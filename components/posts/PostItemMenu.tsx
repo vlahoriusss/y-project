c62c099a8ac56ca2a039b2e7473391f3450a040e
vlahorius <127651785+vlahoriusss@users.noreply.github.com>
2024-04-22 21:08:32 +0300

    5

--- components/posts/PostItemMenu.tsx
+++ components/posts/PostItemMenu.tsx
@@ -1,15 +1,14 @@
-import { BiBlock, BiDotsHorizontal, BiDotsVertical, BiFlag, BiLock } from "react-icons/bi"
-import { useRouter } from "next/router"
-import useLoginModal from "@/hooks/useLoginModal"
-import useCurrentUser from "@/hooks/useCurrentUser"
-import useUser from "@/hooks/useUser"
+import { BiBlock, BiDotsHorizontal, BiFlag } from "react-icons/bi";
+import { BsMicMute, BsPersonAdd, BsPersonDash } from "react-icons/bs";
+import { useRouter } from "next/router";
+import useFollow from "@/hooks/useFollow";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useUser from "@/hooks/useUser";
 import {
     HoverCard,
     HoverCardContent,
     HoverCardTrigger,
-} from "@/components/ui/hover-card"
-import { BsMicMute, BsPersonAdd, BsPersonDash } from "react-icons/bs";
-import useFollow from "@/hooks/useFollow";
+} from "@/components/ui/hover-card";
 
 interface PostItemProps {
     data: Record<string, any>;
@@ -17,45 +16,39 @@ interface PostItemProps {
 }
 
 const PostItemDots: React.FC<PostItemProps> = ({ data = {}, userId }) => {
-    
     const { data: currentUser } = useCurrentUser();
-    const { data: fetchedUser } = useUser(userId);
-    const { isFollowing, toggleFollow } = useFollow(userId);
-    
+    const { data: fetchedUser } = useUser(userId || '');
+    const { isFollowing, toggleFollow } = useFollow(userId || '');
 
     return (
         <div>
             <HoverCard>
                 <HoverCardTrigger>
-                    <BiDotsHorizontal
-                        className='text-neutral-500'
-                        size={20} />
+                    <BiDotsHorizontal className="text-neutral-500" size={20} />
                 </HoverCardTrigger>
                 <HoverCardContent onClick={(event) => event.stopPropagation()} className="flex flex-row flex-wrap gap-5">
-                  
-                   {currentUser?.id !== userId && (
-                        <div className='flex flex-row w-full gap-5' onClick={toggleFollow} >
+                    {currentUser?.id !== userId && (
+                        <div className="flex flex-row w-full gap-5" onClick={toggleFollow}>
                             {isFollowing ? <BsPersonDash size={20}/> : <BsPersonAdd size={20}/> }
                             <p>{isFollowing ? 'Unfollow' : 'Follow'} @{data.user.username}</p>
                         </div>
                     )}
-
-                    <div className='flex flex-row w-full gap-5' ><BsMicMute size={20} />
+                    <div className="flex flex-row w-full gap-5">
+                        <BsMicMute size={20} />
                         <p>Mute @{data.user.username}</p>
                     </div>
-
-                    <div className='flex flex-row w-full gap-5' ><BiBlock size={20} />
+                    <div className="flex flex-row w-full gap-5">
+                        <BiBlock size={20} />
                         <p>Block @{data.user.username}</p>
                     </div>
-
-                    <div className='flex flex-row w-full gap-5'> <BiFlag size={20} />
+                    <div className="flex flex-row w-full gap-5">
+                        <BiFlag size={20} />
                         <p>Report</p>
                     </div>
-
                 </HoverCardContent>
             </HoverCard>
         </div>
-    )
-}
+    );
+};
 
 export default PostItemDots;
