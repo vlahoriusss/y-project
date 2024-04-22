a0f7d2eedbf4ed5a2639b79cdb0a00cd2ee46570
vlahorius <127651785+vlahoriusss@users.noreply.github.com>
2024-04-22 16:12:02 +0300

    layout fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,13 +13,7 @@ const Layout: React.FC<LayoutProps> = ({children}) => {
         <div className="h-screen bg-stone-950 {GeistSans.className}">
             <div className="container h-full mx-auto xl:px-30 max-w-6xl">
                 <div className="grid grid-cols-1 md:grid-cols-4 h-full">
-                    <div className="md:col-span-1 md:flex md:flex-col md:justify-between">
                         <Sidebar />
-                        <div className="md:hidden">
-                            <SupportBar />
-                            <FollowBar />
-                        </div>
-                    </div>
                     <div className="
                         col-span-1 md:col-span-3
                         lg:col-span-2 
