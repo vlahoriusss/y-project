d1a3c7fbd163dd7053020027cce01aaee87b06bf
vlahorius <127651785+vlahoriusss@users.noreply.github.com>
2024-05-07 21:19:21 +0300

    mistake

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -10,7 +10,7 @@ interface ModalProps {
     title?: string;
     body?: React.ReactElement;
     footer?: React.ReactElement;
-    actionLabel?: string;
+    actionLabel: string;
     disabled?: boolean;
 }
 
